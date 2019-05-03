9adf2d56e2e2bd9ca9e67bed0ba84a09f59f8aec
Tuomas Puro <tuomas.puro@esedu.fi>
2019-05-03 14:25:18 +0300

    Login/logout functionality

--- layout/nav.inc.php
+++ layout/nav.inc.php
@@ -3,13 +3,21 @@
     <nav class="main-nav">
         <ul>
             <li>
-                <a href="register.php">Register</a>
+                <a href="show_users.php">Users</a>
             </li>
             <li>
-                <a href="login.php">Login</a>
+                <a href="lists.php">Lists</a>
             </li>
         </ul>
     </nav>
+    <div class="logout">
+        <?php if (isset($_SESSION['username'])): ?>
+            <a href="logout.php">Log out <?php echo $_SESSION['username']; ?></a>  
+        <?php else: ?>
+            <a href="login.php">Login</a> | 
+            <a href="register.php">Register</a>
+        <?php endif; ?>
+    </div>
 </header>
     
 <main class="container">
--- login.php
+++ login.php
@@ -4,33 +4,41 @@ session_start();
 include "layout/layout_functions.php";
 echo_header("Login");
 
+if (isset($_GET['message'])) { $message = $_GET['message']; }
+
 if (isset($_POST['login_button']))
 {
     include "pdo-connection.php";
 
     $data = [
-        'username' => $username,
-        'password' => $password,
-        'email' => $email
+        'username' => $_POST['username']
     ];
 
-    $sql = "INSERT INTO users (username, users.password, email) VALUES (:username, :password, :email)";
+    $sql = "SELECT password FROM users WHERE username = :username";
     $stmt= $pdo->prepare($sql);
+    
     if ($stmt->execute($data)) {
-        $message = "Register succesfull";
-        $_SESSION['user_data'] = $data;
+        $result = $stmt->fetch();
+        
+        // Compare password hash 
+        if (password_verify($_POST['password'], $result['password'])) {
+            $_SESSION['username'] = $_POST['username'];
+            $message = "Register succesfull";    
+            header('Location: show_users.php?message='.urlencode($message));
+        } else {
+            $message = "Invalid password";
+        }
+        
     } else {
         $message = "Register unsuccesfull!";
     }
 
 }
 
-
-
 ?>
 
     <?php if (isset($message)): ?>
-        <p><?php echo $message; ?></p>
+        <p class="error"><?php echo $message; ?></p>
     <?php endif; ?>
     
     <h2>Login</h2>
